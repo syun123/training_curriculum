d3c5d6ba231b43b20d6f8cdbdfce86ec3772be22
syun123 <hashun2801@ezweb.ne.jp>
2021-06-08 11:29:19 +0900

    Issue4

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -8,14 +8,16 @@ class CalendarsController < ApplicationController
 
   # 予定の保存
   def create
+    # binding.pry
     Plan.create(plan_params)
+    # Plan.create(params.require(:calendars).permit(:date, :plan))
     redirect_to action: :index
   end
 
   private
 
   def plan_params
-    params.require(:calendars).permit(:date, :plan)
+    params.require(:plan).permit(:date, :plan)
   end
 
   def getWeek
